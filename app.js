6d2499551ebcc58851b21354a02b03acb7bbab27
pitchkeys <altheat808@gmail.com>
2022-11-03 21:39:36 -1000

    testing this on heroku

--- app.js
+++
@@ -1,6 +0,0 @@
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => {
-    let x = JSON.stringify(json);
-    document.getElementById('content').innerHTML = x;
-  })
